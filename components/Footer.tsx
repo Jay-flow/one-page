5b0bc04015fc01a47359da552c3259f157f5aee2
jay-flow <kimpetro153@gmail.com>
2021-01-27 19:07:08 +0900

    [Ehnace] Remove rule simple-import-sort/sort

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,9 @@ const Footer = (): JSX.Element => {
     return (
         <footer className="flex flex-col items-center justify-center space-y-10 text-white bg-gray-900 h-1/3">
             <div className="flex space-x-10">
-                <a href="">
-                    <FontAwesomeIcon icon={faFacebook} size="3x" className="text-white" />
-                </a>
-                <a href="">
-                    <FontAwesomeIcon icon={faInstagram} size="3x" className="text-white" />
-                </a>
-                <a href="">
-                    <FontAwesomeIcon icon={faTwitter} size="3x" className="text-white" />
-                </a>
+                <FontAwesomeIcon icon={faFacebook} size="3x" className="text-white" />
+                <FontAwesomeIcon icon={faInstagram} size="3x" className="text-white" />
+                <FontAwesomeIcon icon={faTwitter} size="3x" className="text-white" />
             </div>
             <div className="text-center">
                 <h1>Copyright Gmeasying™ All Rights Reserved.</h1>
